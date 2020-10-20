63db233fd82f5fa3af2df33bf9a57f19dce40f3b
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-10-20 19:11:52 +0300

    Add check for null.

--- basic/binary-search/binary-search.test.js
+++ basic/binary-search/binary-search.test.js
@@ -4,4 +4,6 @@ test('properly find the index of the desired item', () => {
     const list = [1, 3, 4, 5, 7, 9, 13];
 
     expect(binarySearch(list, 3)).toBe(1);
+    expect(binarySearch(list, 3)).not.toBeNull();
+    expect(binarySearch(list, 8)).toBeNull();
 });
