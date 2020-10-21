efbe3c92629655fac50f36970a0bddd1f8e05910
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-10-22 00:20:20 +0300

    Change test description.

--- 01-binary-search/binary-search.test.js
+++ 01-binary-search/binary-search.test.js
@@ -4,7 +4,6 @@ test('should find the index of the searched element', () => {
     const list = [1, 3, 4, 5, 7, 9, 13];
 
     expect(binarySearch(list, 3)).toBe(1);
-    expect(binarySearch(list, 3)).not.toBeNull();
 });
 
 test('should return null instead of the index of the searched element', () => {
