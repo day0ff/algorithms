977ef02f33831b4bd09aabf300f6a19c82f26c6e
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-10-21 23:11:11 +0300

    Separate test for null. Change project structure.

---
+++ 01-binary-search/binary-search.test.js
@@ -0,0 +1,14 @@
+const binarySearch = require('./binary-search');
+
+test('should find the index of the searched element', () => {
+    const list = [1, 3, 4, 5, 7, 9, 13];
+
+    expect(binarySearch(list, 3)).toBe(1);
+    expect(binarySearch(list, 3)).not.toBeNull();
+});
+
+test('should return null instead of the index of the searched element', () => {
+    const list = [2, 6, 8, 10, 11];
+
+    expect(binarySearch(list, 5)).toBeNull();
+});
